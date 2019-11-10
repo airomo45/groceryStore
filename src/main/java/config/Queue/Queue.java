4492c1eaefd7dd96ddb01d8bfcb58ede9d5cc124
Romo <annurul.islam@bluecrossmn.com>
2019-11-10 15:48:03 -0600

    Added the fucntions for queue class

--- src/main/java/config/Queue/Queue.java
+++ src/main/java/config/Queue/Queue.java
@@ -2,6 +2,28 @@ package config.Queue;
 
 import structures.QueueInterface;
 
-public class Queue implements QueueInterface {
+public class Queue<T> implements QueueInterface {
+    public T dequeue(){
+        return null;
+    }
 
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public QueueInterface enqueue(Object elem) {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
